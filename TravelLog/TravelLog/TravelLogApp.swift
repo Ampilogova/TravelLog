56b43b63691ad655aaf6d70601aadad37cca58c5
Tanya Ampilogova <ampi.tatyana@gmail.com>
2026-03-03 14:52:07 -0800

    main

---
+++ TravelLog/TravelLog/TravelLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelLogApp.swift
+//  TravelLog
+//
+//  Created by Tatiana Ampilogova on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TravelLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
