c46ae148e6bdaf8370c3182597d3f1808ddeffd6
Tanya Ampilogova <ampi.tatyana@gmail.com>
2026-03-18 17:33:25 -0700

    test delete country

--- TravelLog/TravelLogUITests/TravelLogUITests.swift
+++ TravelLog/TravelLogUITests/TravelLogUITests.swift
@@ -33,6 +33,25 @@ final class TravelLogUITests: XCTestCase {
         app/*@START_MENU_TOKEN@*/.staticTexts["Americas"]/*[[".otherElements.staticTexts[\"Americas\"]",".staticTexts[\"Americas\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
         app/*@START_MENU_TOKEN@*/.staticTexts["USA"]/*[[".otherElements.staticTexts[\"USA\"]",".staticTexts[\"USA\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
     }
+    func testDelete() {
+        let app = XCUIApplication()
+        app.activate()
+        let element = app.buttons["AddCountryView"].firstMatch
+        element.tap()
+        app/*@START_MENU_TOKEN@*/.textFields["Country"]/*[[".otherElements.textFields[\"Country\"]",".textFields[\"Country\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        app/*@START_MENU_TOKEN@*/.textFields["Country"]/*[[".otherElements",".textFields[\"Russia\"]",".textFields[\"Country\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.typeText("Russia")
+        
+        let element2 = app/*@START_MENU_TOKEN@*/.textFields["Memory"]/*[[".otherElements.textFields[\"Memory\"]",".textFields[\"Memory\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch
+        element2.tap()
+        element2.tap()
+        app/*@START_MENU_TOKEN@*/.textFields["Memory"]/*[[".otherElements",".textFields[\"Goo\"]",".textFields[\"Memory\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.typeText("good")
+        app/*@START_MENU_TOKEN@*/.buttons["Increment"]/*[[".steppers",".buttons[\"Rating: 4, Increment\"]",".buttons[\"Increment\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.doubleTap()
+        app/*@START_MENU_TOKEN@*/.buttons["Increment"]/*[[".steppers",".buttons[\"Rating: 5, Increment\"]",".buttons[\"Increment\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        element.tap()
+        app.cells/*@START_MENU_TOKEN@*/.firstMatch/*[[".containing(.other, identifier: nil).firstMatch",".firstMatch"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeLeft()
+        app.buttons["Delete"].firstMatch.tap()
+        
+    }
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
